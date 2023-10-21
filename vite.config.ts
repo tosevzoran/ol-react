39c081f7f5c50c6b2574d156bc96982352611d97
zoki <tosevzoran@gmail.com>
2023-10-21 16:51:23 +0200

    generate types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
@@ -19,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
 });
