f25d907c14ea8202593f12e6f7d41ff37a5f4a3b
zoki <tosevzoran@gmail.com>
2023-10-21 16:23:21 +0200

    setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/"),
+      name: "ol-react",
+      fileName: (format) => `ol-react.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        sourcemap: true,
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
 });
